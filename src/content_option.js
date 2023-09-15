025bd5c5631a0b0b4682a704d068bb392ebb666f
murshidmp <murshidmp4@gmail.com>
2023-09-15 16:39:53 +0530

    Configure React app for deployment to GitHub Pages

---
+++ src/content_option.js
@@ -0,0 +1,162 @@
+const logotext = "MDev";
+const meta = {
+    title: "Mohammed Murshid",
+    description: "I’m Murshid Full stack devloper,currently working as part of a team to implement new features, as well as make modifications and fix bugs in existing code",
+};
+
+const introdata = {
+    title: "I’m Mohammed Murshid",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop Web apps",
+    },
+    description: "As a full-stack developer, I work as part of a team to implement new features, as well as make modifications and fix bugs in existing code",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "About my self",
+    aboutme: "Hello, my name is Mohammed Murshid, and I’m a full stack developer. With Hands on  experience in web development, I have a strong command over both front-end and back-end technologies, allowing me to build complete and scalable applications.",
+};
+const worktimeline = [
+    {
+        jobtitle: "Full Stack Developer",
+        where: "",
+        date: "2023",
+    },
+    {
+        jobtitle: "NodeJs Developer",
+        where: "",
+        date: "2022",
+    },
+    {
+        jobtitle: "Web designer",
+        where: "",
+        date: "2022",
+    },
+];
+
+const skills = [{
+        name: "Node js",
+        value: 50,
+    },
+    {
+        name: "Javascript",
+        value: 50,
+    },
+    {
+        name: "React",
+        value: 50,
+    },
+    {
+        name: "Express",
+        value: 50,
+    },
+    {
+        name: "MongoDB",
+        value: 50,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "passionate about creating intuitive user interfaces that provide an exceptional user experience. I pay great attention to detail, ensuring that the websites or applications I develop are responsive, visually appealing, and accessible across different devices and browsers",
+    },
+    {
+        title: "Web Apps",
+        description: "can design efficient database structures and write optimized queries. I’m familiar with RESTful APIs and know how to integrate them into applications to enable seamless communication between the front end and back end.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/id/1/400?grayscale",
+        description: "Task Manager APP",
+        link: "https://github.com/murshidmp/Task_Manager_App",
+    },
+    {
+        img: "https://picsum.photos/id/2/400/800/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/id/3/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/id/5/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/id/9/400/300/?grayscale",
+        description: "Store API",
+        link: "https://github.com/murshidmp/STORE_API",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "murshidmp4@gmail.com",
+    YOUR_FONE: "+91 8848581779",
+    description: "Should you need any further information, please do not hesitate to contact me.",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_uvy7j9p",
+    YOUR_TEMPLATE_ID: "template_jcuks4d",
+    YOUR_USER_ID: "huq6WBSlhq3Ig87hO",
+};
+
+const socialprofils = {
+    github: "https://github.com/murshidmp",
+    linkedin: "https://www.linkedin.com/in/mohammed-murshid-m-9209a586/",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
